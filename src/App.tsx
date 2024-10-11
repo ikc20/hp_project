dc4698f906b6b9465991370e3865e1bc2bd8796b
Ikram Chahdi <120470168+ikc20@users.noreply.github.com>
2024-10-11 17:50:54 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,45 @@
 import React from 'react';
-import { Alert } from 'react-native';
-import Login from './screens/LoginScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Login from './Autho/Login';
+import SignUp from './Autho/SignUp';
+import Home from './Autho/Home';
+import Profile from './Autho/Profile';
+import ForgotPasswordScreen from './Autho/ForgotPassword'; // Importer l'écran de réinitialisation du mot de passe
 
+const Stack = createStackNavigator();
 
-const App: React.FC = () => {
-  const handleLogin = (email: string, password: string) => {
-    
-    if (email === 'test@example.com' && password === 'password123') {
-      Alert.alert('Connexion réussie', `Bienvenue, ${email}!`);
-    } else {
-      Alert.alert('Erreur', 'Email ou mot de passe incorrect');
-      
-    }
-  };
-
+export default function App() {
   return (
-    
-    <Login onLogin={handleLogin} navigation={undefined} />
+    <NavigationContainer>
+      <Stack.Navigator 
+        initialRouteName="Login" 
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#6C63FF', // Couleur de fond de l'en-tête
+          },
+          headerTintColor: '#fff', // Couleur du texte de l'en-tête
+          headerTitleStyle: {
+            fontWeight: 'bold', // Style du titre
+          },
+        }}
+      >
+        <Stack.Screen 
+          name="Login" 
+          component={Login} 
+          options={{ title: 'Welcome Back' }} // Titre pour l'écran de connexion
+        />
+        <Stack.Screen 
+          name="SignUp" 
+          component={SignUp} 
+          options={{ title: 'Create Account' }} // Titre pour l'écran d'inscription
+        />
+        <Stack.Screen 
+          name="ForgotPassword" 
+          component={ForgotPasswordScreen} 
+          options={{ title: 'Reset Password' }} // Titre pour l'écran de réinitialisation du mot de passe
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-};
-
-export default App;
+}
